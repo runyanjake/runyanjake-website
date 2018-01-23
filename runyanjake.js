e00746e2e83dffb1fe9cd89c87e384c16708d746
Jacob Runyan <jmrunyan@ucsc.edu>
2018-01-22 18:56:16 -0800

    Gave FileIO a shot, met with failure. Will try again another day

--- runyanjake.js
+++ runyanjake.js
@@ -1,3 +1,34 @@
+/*
+ * 
+ * 
+ */
+
+//****Global Variables****//
+var numVisitors = 0;
+
+
+//****Functions****//
+
 function initialize(){
 	console.log("Initializing website.");
+	recordVisit();
+}
+
+//Checks if browser fully supports the File API
+//from tutorial at https://www.html5rocks.com/en/tutorials/file/dndfiles/
+function checkCompatabilityFileAPI(){
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		// Great success! All the File APIs are supported.
+	} else {
+		alert('The File APIs are not fully supported in this browser.');
+	}
+}
+
+function recordVisit(){
+	console.log("Number of visitors: " + getNumberVisitors());
+}
+
+function getNumberVisitors(){
+	return -1;
 }
